804fae27202e6f2825bf174d6f507b628e0e96db
Sebastian <sebafehlandt@gmail.com>
2018-02-08 16:31:00 -0300

    Remove channel redis from settings in order to fix issues while testing

--- channels2_example/settings.py
+++ channels2_example/settings.py
@@ -121,18 +121,18 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-redis_host = os.environ.get('REDIS_HOST', 'localhost')
-
-# Channel layer definitions
-# http://channels.readthedocs.org/en/latest/deploying.html#setting-up-a-channel-backend
-CHANNEL_LAYERS = {
-    "default": {
-        # This example app uses the Redis channel layer
-        # implementation asgi_redis
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [(redis_host, 6379)],
-        }
-    },
-}
+# redis_host = os.environ.get('REDIS_HOST', 'localhost')
+#
+# # Channel layer definitions
+# # http://channels.readthedocs.org/en/latest/deploying.html#setting-up-a-channel-backend
+# CHANNEL_LAYERS = {
+#     "default": {
+#         # This example app uses the Redis channel layer
+#         # implementation asgi_redis
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [(redis_host, 6379)],
+#         }
+#     },
+# }
 ASGI_APPLICATION = "channels2_example.routing.application"
